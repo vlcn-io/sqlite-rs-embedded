b0ced62a9dfe4d9eecfd7f1a22136dccf3d75198
Matt <1009003+tantaman@users.noreply.github.com>
2023-08-23 12:05:10 -0400

    expose authorizer action codes

--- sqlite_nostd/src/nostd.rs
+++ sqlite_nostd/src/nostd.rs
@@ -17,6 +17,45 @@ use num_traits::FromPrimitive;
 pub use sqlite3_allocator::*;
 pub use sqlite3_capi::*;
 
+// https://www.sqlite.org/c3ref/c_alter_table.html
+#[derive(FromPrimitive, PartialEq, Debug)]
+pub enum ActionCode {
+    COPY = 0,
+    CREATE_INDEX = 1,
+    CREATE_TABLE = 2,
+    CREATE_TEMP_INDEX = 3,
+    CREATE_TEMP_TABLE = 4,
+    CREATE_TEMP_TRIGGER = 5,
+    CREATE_TEMP_VIEW = 6,
+    CREATE_TRIGGER = 7,
+    CREATE_VIEW = 8,
+    DELETE = 9,
+    DROP_INDEX = 10,
+    DROP_TABLE = 11,
+    DROP_TEMP_INDEX = 12,
+    DROP_TEMP_TABLE = 13,
+    DROP_TEMP_TRIGGER = 14,
+    DROP_TEMP_VIEW = 15,
+    DROP_TRIGGER = 16,
+    DROP_VIEW = 17,
+    INSERT = 18,
+    PRAGMA = 19,
+    READ = 20,
+    SELECT = 21,
+    TRANSACTION = 22,
+    UPDATE = 23,
+    ATTACH = 24,
+    DETACH = 25,
+    ALTER_TABLE = 26,
+    REINDEX = 27,
+    ANALYZE = 28,
+    CREATE_VTABLE = 29,
+    DROP_VTABLE = 30,
+    FUNCTION = 31,
+    SAVEPOINT = 32,
+    RECURSIVE = 33,
+}
+
 #[derive(FromPrimitive, PartialEq, Debug)]
 pub enum ResultCode {
     OK = 0,
